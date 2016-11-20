87f54b44f3451966f67ae8ee2c44db7a7c08af96
Paulo Vieira <paulovieira@gmail.com>
2016-11-20 05:55:44 +0000

    update

--- plugins/dashboard/app/state/StateV.js
+++ plugins/dashboard/app/state/StateV.js
@@ -11,10 +11,23 @@ var StateV = Mn.LayoutView.extend({
     initialize: function(){
         
         var pivotsC = new PivotsC(ctx.pivots);
+        pivotsC.each(function(pivotM){
+
+            if(pivotM.get('on')){
+                pivotM.set('stateText', 'Ligado');
+                pivotM.set('stateColor', 'green');
+            }
+            else{
+                pivotM.set('stateText', 'Desligado');
+                pivotM.set('stateColor', 'orange');
+            }
+        });
 
         this.model = new Backbone.Model({
             pivotsC: pivotsC
         });
+
+
 
     },
 
